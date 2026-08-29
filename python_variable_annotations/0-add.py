774197d98fb44a8ec886caeb51cd22dd9a4b2289
laila-khalid <13252@holbertonstudents.com>
2026-08-29 20:34:12 +0300

    0. Basic annotations - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This module provides a basic addition function with type annotations."""
+
+
+def add(a: float, b: float) -> float:
+    """Adds two float numbers and returns the sum as a float."""
+    return a + b
