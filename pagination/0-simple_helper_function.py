04f930b6381030385dbc5d32861ceac53986edca
laila-khalid <13252@holbertonstudents.com>
2026-09-12 22:42:06 +0300

    Fix executable permissions and strict PEP8 formatting

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python3
-"""Simple helper function module for pagination."""
+"""Simple helper function for pagination."""
 from typing import Tuple
 
 
 def index_range(page: int, page_size: int) -> Tuple[int, int]:
-    """Calculate start and end indexes for pagination parameters."""
-    start = (page - 1) * page_size
-    end = page * page_size
-    return (start, end)
+    """Return a tuple of start and end indexes for pagination."""
+    return ((page - 1) * page_size, page * page_size)
