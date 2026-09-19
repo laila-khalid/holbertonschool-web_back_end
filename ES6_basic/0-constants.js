b1dc60f7aa502c8f6ac6ece571308660b3ae4229
laila-khalid <13252@holbertonstudents.com>
2026-09-19 22:59:55 +0300

    fix task 0

--- ES6_basic/0-constants.js
+++ ES6_basic/0-constants.js
@@ -8,7 +8,7 @@ export function getLast() {
 }
 
 export function taskNext() {
-  const combination = 'But sometimes const';
+  let combination = 'But sometimes let';
   combination += getLast();
 
   return combination;
